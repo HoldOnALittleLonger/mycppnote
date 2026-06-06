90c9a4f3064dcba2e8fda81d4db8b1a942f0cb72
Zephyr <emailunspecified@false.com>
2026-06-06 16:35:01 +0800

    251

--- meta_binary.cc
+++ meta_binary.cc
@@ -1,44 +1,39 @@
+#include <type_traits>
+
 #include <cstddef>
 
+// metafunctions to check whether @N is a good binary format.
 template<unsigned int N>
-constexpr bool is_greater_than_one(void)
-{
-  return N > 1;
-}
-
-template<unsigned int N>
-constexpr bool check_if_bad_format(void)
-{
-  return is_greater_than_one<N % 10>() || check_if_bad_format<N / 10>();
-}
-
-template<>
-constexpr bool check_if_bad_format<1>(void)
-{
-  return false;
-}
-
-template<unsigned int N>
-constexpr unsigned int is_bad_format(void)
-{
-  /**
-   * We intentionally let the result is "1 / 0",
-   * because we dont want bad format can pass compilation.
-   */
-  return check_if_bad_format<N>() ? 1 / 0 : N;
-}
-
-template<unsigned int _number>
-struct binary {
-  static constexpr unsigned int value =
-    binary<is_bad_format<_number>() / 10>::value << 1 | _number % 10;
+struct is_good_binary_format : public std::true_type {
+  static constexpr bool value =
+    std::true_type::value && N % 10 <= 1 && is_good_binary_format<N / 10>::value;
 };
 
 template<>
-struct binary<0> {
+struct is_good_binary_format<1> : public std::true_type {
+  using std::true_type::value;
+};
+
+template<>
+struct is_good_binary_format<0> : public std::true_type {
+  using std::true_type::value;
+};
+
+template<unsigned int _number, bool good_format = is_good_binary_format<_number>::value>
+struct binary { };
+
+// only defined when @_number is a good binary format.
+template<unsigned int _number>
+struct binary<_number, true> {
+  static constexpr unsigned int value = binary<_number / 10>::value << 1 | _number %10;
+};
+
+template<>
+struct binary<0, true> {
   static constexpr unsigned int value = 0;
 };
 
+// use static_assert(false) to print value.
 template<unsigned int X>
 struct PrintX {
   PrintX()
@@ -47,10 +42,9 @@ struct PrintX {
   }
 };
 
-
 int main(void)
 {
-  //  auto x = binary<202>::value;
+  // auto a = binary<202>::value;
   constexpr auto x = binary<11111111>::value;
   PrintX<x> print_it;
   return 0;
