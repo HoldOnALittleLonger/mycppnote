c049598234a40b1fc3baaa216a8cbbf4746f5070
Zephyr <emailunspecified@false.com>
2026-06-11 18:08:04 +0800

    258

---
+++ metaprogram_int_forward_iterator.cc
@@ -0,0 +1,64 @@
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/plus.hpp>
+#include <boost/mpl/minus.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/iterator_tags.hpp>
+#include <boost/mpl/next.hpp>
+#include <boost/mpl/deref.hpp>
+#include <boost/type_traits.hpp>
+
+using namespace boost::mpl;
+
+template<typename _SeqType>
+struct ud_int_forward_iterator {
+  typedef forward_iterator_tag category;
+};
+
+namespace boost { namespace mpl {
+    template<typename _SeqType>
+    struct deref<ud_int_forward_iterator<_SeqType>> {
+      typedef typename _SeqType::type type;
+    };
+    
+    template<typename _SeqType>
+    struct next<ud_int_forward_iterator<_SeqType>> {
+      typedef ud_int_forward_iterator<typename next<_SeqType>::type> type;
+    };
+
+  }}
+
+
+int main(void)
+{
+  typedef ud_int_forward_iterator<int_<0>> int0_fiter;
+  BOOST_STATIC_ASSERT((
+                       equal_to<deref<int0_fiter>::type, int_<0>>::value,
+                       "should be same value 0"
+                       ));
+  typedef next<int0_fiter>::type int1_fiter;
+  BOOST_STATIC_ASSERT((
+                       equal_to<deref<int1_fiter>::type, int_<1>>::value,
+                       "should be same value 1"
+                       ));
+
+  BOOST_STATIC_ASSERT((
+                       boost::is_same<
+                       plus<deref<int0_fiter>::type, int_<1>>,
+                       int_<1>::type
+                       >::value,
+                       "should be same type int_<1>"
+                       ));
+  BOOST_STATIC_ASSERT((
+                       boost::is_same<
+                       minus<deref<int1_fiter>::type, int_<1>>::type,
+                       deref<int0_fiter>::type
+                       >::value,
+                       "should be same type int_<0>"
+                       ));
+  
+  static_assert(deref<int0_fiter>::type::value != int_<0>::value, "<= @value is ");
+
+  
+  return 0;
+}
